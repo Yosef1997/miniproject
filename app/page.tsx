d81354b838b01800909e403be3152a3a7f0d3918
Yosef1997 <yosefsitumorang97@gmail.com>
2024-06-16 22:13:57 +0700

    style(global): set padding and margin 0

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen'>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </main>
   )
