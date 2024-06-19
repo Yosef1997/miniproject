a13373e900781efb4b25baccb376b197df322744
Yosef1997 <yosefsitumorang97@gmail.com>
2024-06-20 00:57:52 +0700

    feat:slicing layout landing page mobile done

--- app/(main)/_components/EventCard.tsx
+++ app/(main)/_components/EventCard.tsx
@@ -1,14 +1,35 @@
+import { BsFillStarFill } from "react-icons/bs"
 import Image from "next/image"
 import React from "react"
 import EventImg from "@/public/hero.webp"
 
-const EventCard = () => {
+interface EventCardProps {
+  rating: string
+  title: string
+  date: string
+  location: string
+  price: string
+}
+
+const EventCard: React.FC<EventCardProps> = (props) => {
   return (
-    <div>
-      <div>
-        <div className='relative'>
-          <Image src={EventImg} width={400} height={240} alt='event' />
+    <div className='w-[200px] bg-white rounded-md'>
+      <div className='relative'>
+        <Image
+          src={EventImg}
+          alt='event'
+          className='h-[120px] w-[200px] object-cover rounded-md'
+        />
+        <div className='bg-white flex items-center gap-x-2 absolute top-2 left-2 p-2 rounded-md'>
+          <BsFillStarFill color='#F4B740' />
+          <p className='text-xs'>{props.rating}</p>
         </div>
+      </div>
+      <div className='p-6'>
+        <h2 className='font-bold text-title'>{props.title}</h2>
+        <p className='text-body text-sm'>{props.date}</p>
+        <p className='text-body text-sm'>{props.location}</p>
+        <p className='text-body text-sm font-semibold'>IDR {props.price}</p>
       </div>
     </div>
   )
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,16 +1,127 @@
+"use client"
 import Image from "next/image"
 import EventCard from "./_components/EventCard"
 import Hero from "@/public/hero.webp"
+import Link from "next/link"
+import * as yup from "yup"
+import { Field, Form, Formik, FormikProps } from "formik"
+
+const landingSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email address format")
+    .required("Email is required"),
+})
+
+interface landingValues {
+  email: string
+}
 
 export default function Home() {
+  const initialValues: landingValues = {
+    email: "",
+  }
   return (
-    <main className='flex min-h-screen'>
-      <div>
-        <div className='px-[104px] w-fit'>
-          <Image className='rounded-2xl' src={Hero} alt='hero' />
+    <div className='bg-background'>
+      <div className='relative'>
+        <Image
+          src={Hero}
+          alt='hero'
+          className='w-full lg:h-[80vh] object-cover'
+        />
+        <div className='absolute flex flex-col justify-between inset-0 bg-primary bg-opacity-40  p-6 lg:p-20'>
+          <h2 className='text-white md:text-7xl font-bold'>
+            DISCOVER <br /> POPULAR <br /> EVENTS TODAY
+          </h2>
+          <button
+            type='button'
+            title='Explore Events'
+            className='bg-primary text-white-btn font-semibold p-2 lg:py-3 lg:px-8 rounded-md w-fit'
+          >
+            Explore Events
+          </button>
         </div>
-        <EventCard />
       </div>
-    </main>
+      <div className='pt-12 pb-14'>
+        <div className='flex items-center justify-between px-6 pb-8'>
+          <h2 className='text-lg font-bold text-primary'>Popular Events</h2>
+          <Link href={"/popularevents"} className='font-semibold text-primary'>
+            View All
+          </Link>
+        </div>
+        <div className='ml-6'>
+          <EventCard
+            rating={"4.5"}
+            title={"Self Development Workshop"}
+            date={"Tuesday, 07 July 2020 - 04:30pm"}
+            location={"Grand Convexion Mapura Hotel, Taiwan"}
+            price={"50000.00"}
+          />
+        </div>
+      </div>
+      <div className='pt-12 pb-14 bg-background-v2'>
+        <div className='flex items-center justify-between px-6 pb-8'>
+          <h2 className='text-lg font-bold text-primary'>Upcoming Events</h2>
+          <Link href={"/popularevents"} className='font-semibold text-primary'>
+            View All
+          </Link>
+        </div>
+        <div className='ml-6'>
+          <EventCard
+            rating={"4.5"}
+            title={"Self Development Workshop"}
+            date={"Tuesday, 07 July 2020 - 04:30pm"}
+            location={"Grand Convexion Mapura Hotel, Taiwan"}
+            price={"50000.00"}
+          />
+        </div>
+        <div className='bg-white mx-6 mt-16 px-8 py-12 rounded-md'>
+          <h2 className='font-bold text-primary text-center'>
+            GET UPDATE FOR POPULAR EVENTS EVERYDAY
+          </h2>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={landingSchema}
+            onSubmit={async (values) => {
+              console.log(values)
+            }}
+          >
+            {(props: FormikProps<landingValues>) => {
+              const { values, errors, touched, handleChange } = props
+              console.log(props.values)
+              return (
+                <Form className='lg:flex lg:justify-center lg:items-start lg:gap-x-4 lg:mt-10'>
+                  <div className='flex flex-col my-5 lg:my-0'>
+                    <Field
+                      className='px-4 lg:px-6 py-2 lg:py-5 mt-3 lg:mt-0 rounded-md border border-border-line focus:outline-none'
+                      type='email'
+                      name='email'
+                      onChange={handleChange}
+                      value={values.email}
+                      placeholder='Type your email'
+                    />
+                    {touched.email && errors.email ? (
+                      <div className='text-error text-sm mt-1'>
+                        {errors.email}
+                      </div>
+                    ) : null}
+                  </div>
+                  <button
+                    className='bg-primary text-white font-bold w-full lg:w-fit py-2 lg:px-8 lg:py-5 rounded-md'
+                    type='submit'
+                  >
+                    Join now
+                  </button>
+                </Form>
+              )
+            }}
+          </Formik>
+          <p className='text-placeholder text-center mt-8'>
+            By joining you as a Tickitz member, we will always send you the
+            latest updates via email .
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
