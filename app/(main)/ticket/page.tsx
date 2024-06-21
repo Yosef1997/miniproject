a0e735b953dd0f996c24d29fbd1d05089f555354
Yosef1997 <yosefsitumorang97@gmail.com>
2024-06-21 10:06:50 +0700

    style: add logo tickitz and change padding

--- app/(main)/ticket/page.tsx
+++ app/(main)/ticket/page.tsx
@@ -3,47 +3,69 @@ import Image from "next/image"
 import React, { useContext } from "react"
 import Hero from "@/public/hero.webp"
 import TickitzContext from "@/context/TickitzContext"
+import TickitzWhite from "@/public/tickitz-white.svg"
+
 const page = () => {
   const { event } = useContext(TickitzContext)
 
   return (
     <div className='bg-background px-6 lg:px-16 xl:px-32 py-12'>
       <div className='bg-white rounded-md lg:grid lg:grid-cols-3'>
-        <div className=' relative flex justify-center items-center max-lg:border-b lg:border-r border-border-line border-dashed py-8'>
-          <Image src={Hero} alt='barcode' className='w-[186px] h-[186px]' />
+        <div className=' relative flex flex-col justify-center items-center max-lg:border-b lg:border-r border-border-line border-dashed '>
+          <div className='bg-primary w-full rounded-t-md hidden lg:flex justify-center lg:py-4'>
+            <Image
+              src={TickitzWhite}
+              height={49}
+              width={125}
+              alt='Tickitz-white'
+            />
+          </div>
+          <div className='py-12'>
+            <Image src={Hero} alt='barcode' className='w-[186px] h-[186px]' />
+          </div>
           <div className='absolute hidden lg:block h-[50px] w-[50px] rounded-full bg-background top-0 right-0 translate-x-1/2 -translate-y-1/2' />
           <div className='absolute block lg:hidden h-[50px] w-[50px] rounded-full bg-background -bottom-0 left-0 -translate-x-1/2 translate-y-1/2' />
           <div className='absolute h-[50px] w-[50px] rounded-full bg-background -bottom-0 right-0 translate-x-1/2 translate-y-1/2' />
         </div>
-        <div className='px-6 py-14 flex flex-col gap-y-4'>
-          <div className='grid grid-cols-2 gap-x-6'>
-            <div>
-              <p className='text-label text-sm'>Event</p>
-              <p className='text-title'>{event?.title}</p>
-            </div>
-            <div>
-              <p className='text-label text-sm'>Category</p>
-              <p className='text-title'>{event?.category}</p>
-            </div>
+        <div className='lg:col-span-2'>
+          <div className='bg-primary w-full rounded-t-md hidden lg:flex justify-end px-14 py-4'>
+            <Image
+              src={TickitzWhite}
+              height={49}
+              width={125}
+              alt='Tickitz-white'
+            />
           </div>
-          <div className='grid grid-cols-2 gap-x-6'>
-            <div>
-              <p className='text-label text-sm'>Date</p>
-              <p className='text-title'>{event?.date}</p>
+          <div className='flex flex-col gap-y-4 py-12 px-8 lg:px-14'>
+            <div className='grid grid-cols-2 gap-x-6'>
+              <div>
+                <p className='text-label text-sm'>Event</p>
+                <p className='text-title line-clamp-3'>{event?.title}</p>
+              </div>
+              <div>
+                <p className='text-label text-sm'>Venue</p>
+                <p className='text-title line-clamp-3'>{event?.vanue}</p>
+              </div>
             </div>
-            <div>
-              <p className='text-label text-sm'>Time</p>
-              <p className='text-title'>{event?.time}</p>
+            <div className='grid grid-cols-2 gap-x-6'>
+              <div>
+                <p className='text-label text-sm'>Date</p>
+                <p className='text-title'>{event?.date}</p>
+              </div>
+              <div>
+                <p className='text-label text-sm'>Time</p>
+                <p className='text-title'>{event?.time}</p>
+              </div>
             </div>
-          </div>
-          <div className='grid grid-cols-2 gap-x-6'>
-            <div>
-              <p className='text-label text-sm'>Count</p>
-              <p className='text-title'>3 pcs</p>
-            </div>
-            <div>
-              <p className='text-label text-sm'>Total</p>
-              <p className='text-title'>IDR {event?.price}</p>
+            <div className='grid grid-cols-2 gap-x-6'>
+              <div>
+                <p className='text-label text-sm'>Count</p>
+                <p className='text-title'>3 pcs</p>
+              </div>
+              <div>
+                <p className='text-label text-sm'>Price</p>
+                <p className='text-title'>IDR {event?.price}</p>
+              </div>
             </div>
           </div>
         </div>
