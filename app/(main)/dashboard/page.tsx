f2bee66bfd079f18469eafeb6385644429ba057f
Yosef1997 <yosefsitumorang97@gmail.com>
2024-06-23 15:59:12 +0700

    chore: add env file

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Field, Form, Formik, FormikProps } from "formik"
+import * as yup from "yup"
+
+const page = () => {
+  return <div></div>
+}
+
+export default page
